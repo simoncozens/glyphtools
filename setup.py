29402cee9044c678bb9146ec27997456e61580a1
Simon Cozens <simon@simon-cozens.org>
2022-01-21 13:49:19 +0000

    Bump version: 0.7.4 → 0.8.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/simoncozens/glyphtools',
-    version='0.7.4',
+    version='0.8.0',
     zip_safe=False,
 )
